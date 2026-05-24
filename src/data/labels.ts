50346f7a1713c75b7606caeed7ebee22d4d3ed8f
igna-gnzlz <gonzalezignacio0404@gmail.com>
2026-05-24 13:06:14 -0300

    feat: crear etiquetas como mock data

---
+++ src/data/labels.ts
@@ -0,0 +1,17 @@
+export type Label = {
+  id: string;
+  label: string;
+};
+
+export const labels: Label[] = [
+  { id: "organic", label: "organic" },
+  { id: "vegan", label: "vegan" },
+  { id: "vegetarian", label: "vegetarian" },
+  { id: "gluten-free", label: "gluten-free" },
+  { id: "no-added-sugar", label: "no-added-sugar" },
+  { id: "fair-trade", label: "fair-trade" },
+  { id: "lactose-free", label: "lactose-free" },
+  { id: "palm-oil-free", label: "palm-oil-free" },
+  { id: "high-fiber", label: "high-fiber" },
+  { id: "low-fat", label: "low-fat" },
+];
